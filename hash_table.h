4f78137cbcab4fda5f3403013c7238fb86228986
Kreusch-Leo <kreuschleoanc@gmail.com>
2025-06-19 12:23:48 -0300

    ADD: basic cuckoo hash header

---
+++ hash_table.h
@@ -0,0 +1,63 @@
+// Header para Cuckoo Hash segundo especificacoes para o trabalho II de Algoritmos e Estruturas de Dados III
+// Leonardo Kreusch GRR20245579 - Bacharelado em Ciencia da Computacao - UFPR
+// Ultima modificacao em 19/06/2025
+
+#ifndef __HASH_TABLE__
+#define __HASH_TABLE__
+
+
+struct cuckoo_ht_t {
+    int size;                  // table size (m)
+    void **content_t1;         // pointer a vector of pointers t1
+    void **content_t2;         // pointer a vector of pointers t2
+};
+
+/* Creates a cuckoo hash structure with a given size
+
+    In  : size (m)
+    Out : pointer to struct
+*/
+struct cuckoo_ht_t *create_cuckoo_ht(int size);
+
+
+/* Destroy a given cuckoo ht and frees memory
+    
+    In  : pointer to struct
+    Out : successful operation (0), error (1)
+*/
+int destroy_cuckoo_ht(struct cuckoo_ht_t *t);
+
+/* Adds new data to the ht, 
+    if possible to t1
+    else if possible to t2
+    else error
+    
+    In  : pointer to struct, key value
+    Out : successful operation (0), error (1)
+*/
+int add_content(struct cuckoo_ht_t *t, int k);
+
+/* Searchs for data in the ht, 
+    
+    In  : pointer to struct, key value
+    Out : if exists: pointer to content
+          else: NULL
+*/
+void *search_content(struct cuckoo_ht_t *t);
+
+/* Removes data from the ht, 
+    
+    In  : pointer to struct, key value
+    Out : if exists: pointer to content
+          else: NULL
+*/
+void *remove_content(struct cuckoo_ht_t *t, int k);
+
+/* Prints content from the structure according to the assigment prompt, 
+    
+    In  : pointer to struct
+    Out : void
+*/
+void print_tables(struct cuckoo_ht_t *t);
+
+#endif
